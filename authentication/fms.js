38719b78b4b5a0cba7b34138656710787d9f88ec
Daniel Kleebinder <daniel.kleebinder@austrosoft.at>
2020-01-29 12:06:22 +0100

    Added documentation to the functions

--- authentication/fms.js
+++ authentication/fms.js
@@ -1,6 +1,18 @@
 const hash = require('./md');
 
 
+/**
+ * Generates a signature for the given parameters.
+ *
+ * @param apiVersion API version (e.g.: '1.5')
+ * @param apiKey API key ({string}).
+ * @param salt Salt ({string}).
+ * @param query Query ({string}).
+ * @param httpBody HTTP body (as {string}. use JSON.stringify).
+ * @param timestamp Timestamp in ms since 1970.
+ * @param hashAlgorithm Optional hashing algorithm.
+ * @returns {string}
+ */
 const signatureFn = function (apiVersion, apiKey, salt, query, httpBody = '', timestamp = Date.now(), hashAlgorithm = hash.sha256) {
     const separator = '::';
     const concat =
@@ -15,6 +27,7 @@ const signatureFn = function (apiVersion, apiKey, salt, query, httpBody = '', ti
 };
 
 
+// Available functions exported by this JavaScript module
 module.exports = {
     signature: signatureFn
 };
--- test.js
+++ test.js
@@ -5,10 +5,21 @@ const userInfo = {
     key: 'ef93b9e7914c332c26ffcedf284f7b71033192680da36d237103ecbb60418a61c31d250e5deeb7f2be082b4fe79484111ca64e527b397296b1522e2fd2e26f7a'
 };
 
-const signature = auth.signature('1.5', userInfo.key, 'x883', '/api/ping', '', 33);
+let signature;
+
+// test signature generation
+signature = auth.signature('1.5', userInfo.key, 'x883', '/api/ping', '', 33);
 if (signature !== '1.5:c973090ca6115c804170532333952879f3588e115c21d9fd775bc4e8bf5c54cd') {
     console.log("wrong signature");
     process.exit();
 }
 
+// test date capabilities with body
+signature = auth.signature('1.5', userInfo.key, 'x883', '/api/ping', userInfo);
+if (signature.length === '1.5:c973090ca6115c804170532333952879f3588e115c21d9fd775bc4e8bf5c54cd') {
+    console.log("signatures cannot be equal");
+    process.exit();
+}
+
+// everything worked well
 console.log('all ok');
